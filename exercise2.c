5311f4ea81ec5420740b4b8e0328684830d15935
SonarBoy <Joshua.L99.C@gmail.com>
2018-07-24 23:07:18 -0400

    Initial Commit

---
+++ exercise2.c
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+	int firstNumber;
+	int secondNumber;
+
+	cout << "Enter a Number: " << "\n";
+	cin >> firstNumber;
+
+	cout << "Enter a Number: " << "\n";
+	cin >> secondNumber;
+
+	cout << secondNumber + firstNumber << "\n";
+
+
+
+
+	return 0;
+}
