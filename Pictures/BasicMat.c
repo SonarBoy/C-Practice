092f1ae911ee31785f917011ed6d9daf01caca25
SonarBoy <Joshua.L99.C@gmail.com>
2019-04-04 22:18:18 -0400

    First Clean Up.

--- Pictures/BasicMat.c
+++ Pictures/BasicMat.c
@@ -1,7 +1,6 @@
 #include "opencv2/opencv.hpp"
 #include <iostream>
 using namespace cv;
-
 //http://answers.opencv.org/question/1/how-can-i-get-frames-from-my-webcam/
 
 
@@ -15,6 +14,9 @@ int main(int argc, char** argv)
     Mat A, C;
 
     A = imread(argv[1],IMREAD_COLOR);
+    std::cout << "A = " << std::endl << " " << A << std::endl << " " << std::endl;
+
+
 
     Mat B(A);
 
@@ -29,12 +31,13 @@ int main(int argc, char** argv)
     Mat G;
     A.copyTo(G);
 
-   /* Now modifying F or G will not affect the matrix pointed by the Mat header. What you need to remember forom all this is that:
+    /* Now modifying F or G will not affect the matrix pointed by the Mat header. What you need to remember 
+    forom all this is that:
 
-  Output image allocation for OpenCV functions is automatic (unless specified otherwise).
-  You do not need to think about memory management with OpenCVs C++ interface.
-  The assignment operator and the copy constructor only copies the header.
-  The underlying matrix of an image may be copied using the cv::Mat::clone() and cv::Mat::copyTo() functions. */
+    Output image allocation for OpenCV functions is automatic (unless specified otherwise).
+    You do not need to think about memory management with OpenCVs C++ interface.
+    The assignment operator and the copy constructor only copies the header.
+    The underlying matrix of an image may be copied using the cv::Mat::clone() and cv::Mat::copyTo() functions. */
 
     Mat M(7,2, CV_8UC3, Scalar(0,0,255)); 
     //ROW AND COLOUM COMBONATION.
@@ -42,15 +45,17 @@ int main(int argc, char** argv)
     //UC3 = Unsigned Char Type 8 Bits Long each pixel has 3 for each of the three channels. 
     std::cout << "M = " << std::endl << " " << M << std::endl << " " << std::endl;
 
+
     int sz[3] = {2,2,2};
     Mat L(3,sz, CV_8UC(1), Scalar::all(0));
     //The upper example shows how to create a matrix with more than two dimensions. 
     //Specify its dimension, then pass a pointer containing the size for each dimension and the rest remains the same
 
-    //std::cout << "L = " << std::endl << " " << L << std::endl << " " << std::endl;
-
+    
     M.create(3,4, CV_8UC(2));
     std::cout << "M = "<< std::endl << " "  << M << std::endl << std::endl;
+
+
     //You cannot initialize the matrix values with this construction. It will only reallocate 
     //its matrix data memory if the new size will not fit into the old one.
 
